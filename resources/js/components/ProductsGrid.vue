84d646439905a6e3ff8e43fa083e57aaba0cf479
Matt Brown <22159690+matthewbrown1234@users.noreply.github.com>
2025-11-08 15:47:10 -0700

    moving products to a component

---
+++ resources/js/components/ProductsGrid.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { AgGridVue } from 'ag-grid-vue3';
+import { ref } from 'vue';
+import type { Product } from '@/types';
+
+defineProps<{
+    products: Product[];
+}>();
+
+// Column Definitions: Defines the columns to be displayed.
+const colDefs = ref([
+    {
+        field: 'image',
+        headerName: 'Image',
+        width: 100,
+        cellRenderer: (params: any) => {
+            return `<img src="${params.value}" alt="Product Image" style="width: 50px; height: auto;" />`;
+        },
+    },
+    { filter: true, field: 'id', headerName: 'ID', width: 80 },
+    { filter: true, field: 'title', headerName: 'Title', width: 300 },
+    { filter: true, field: 'price', headerName: 'Price', width: 100 },
+    { filter: true, field: 'description', headerName: 'Desc', width: 900 },
+]);
+</script>
+
+<template>
+    <AgGridVue
+        style="height: 100%"
+        :rowData="products"
+        :columnDefs="colDefs"
+    ></AgGridVue>
+</template>
+
+<style scoped></style>
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -3,9 +3,8 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head, usePage } from '@inertiajs/vue3';
-import { AgGridVue } from 'ag-grid-vue3'; // Vue Data Grid Component
-import { ref } from 'vue';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import ProductsGrid from '@/components/ProductsGrid.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -15,17 +14,6 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 const page = usePage();
 const products = page.props.products;
-
-// Column Definitions: Defines the columns to be displayed.
-const colDefs = ref([
-    { field: 'image',headerName: 'Image', width: 100, cellRenderer: (params: any) => {
-        return `<img src="${params.value}" alt="Product Image" style="width: 50px; height: auto;" />`;
-    } },
-    { filter: true, field: 'id', headerName: 'ID', width: 80 },
-    { filter: true, field: 'title',headerName: 'Title', width: 300 },
-    { filter: true, field: 'price',headerName: 'Price', width: 100 },
-    { filter: true, field: 'description',headerName: 'Desc', width: 900 },
-]);
 </script>
 
 <template>
@@ -55,11 +43,7 @@ const colDefs = ref([
             <div
                 class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border"
             >
-                <AgGridVue
-                    style="height: 100%"
-                    :rowData="products"
-                    :columnDefs="colDefs"
-                ></AgGridVue>
+                <ProductsGrid :products="products" />
             </div>
         </div>
     </AppLayout>
