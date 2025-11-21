c17a7333a67700ea224727cda0b3504a18aca24a
Matt Brown <22159690+matthewbrown1234@users.noreply.github.com>
2025-11-21 15:07:56 -0700

    cleaning up table UI

--- resources/js/components/OrdersGrid.vue
+++ resources/js/components/OrdersGrid.vue
@@ -2,7 +2,7 @@
 import { EditButton, StatusCell } from '@/components/ui/grid/cells';
 import type { Order } from '@/types';
 import { router } from '@inertiajs/vue3';
-import { ColDef } from 'ag-grid-community';
+import { AutoSizeStrategy, ColDef } from 'ag-grid-community';
 import { AgGridVue } from 'ag-grid-vue3';
 import { ref } from 'vue';
 
@@ -10,22 +10,24 @@ const { orders } = defineProps<{
     orders: Order[];
 }>();
 
+const autoSizeStrategy: AutoSizeStrategy = {
+    type: 'fitGridWidth',
+    defaultMinWidth: 150,
+};
 // Column Definitions: Defines the columns to be displayed.
 const colDefs = ref<ColDef[]>([
-    { filter: true, field: 'user.name', headerName: 'Name', width: 300 },
-    { filter: true, field: 'user.email', headerName: 'Email', width: 300 },
+    { filter: true, field: 'user.name', headerName: 'Name' },
+    { filter: true, field: 'user.email', headerName: 'Email' },
     {
         filter: true,
         field: 'order_date',
         headerName: 'Order Date',
-        width: 300,
         cellDataType: 'dateTimeString',
     },
     {
         filter: true,
         field: 'status',
         headerName: 'Status',
-        width: 120,
         cellRenderer: StatusCell,
     },
     {
@@ -36,7 +38,10 @@ const colDefs = ref<ColDef[]>([
                 router.visit(`/orders/${order.id}`);
             },
         },
-        width: 70,
+        maxWidth: 95,
+        colId: 'actions',
+        pinned: 'right',
+        resizable: false,
     },
 ]);
 </script>
@@ -44,6 +49,7 @@ const colDefs = ref<ColDef[]>([
 <template>
     <AgGridVue
         style="height: 100%"
+        :autoSizeStrategy="autoSizeStrategy"
         :rowData="orders"
         :columnDefs="colDefs"
     ></AgGridVue>
